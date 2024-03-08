63cab539c89c9180d9f9c649447450f0e1386391
satomitakahashi <takahashi.satomi@woven.toyota>
2024-03-08 16:55:36 +0900

    submit

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,11 +1,14 @@
 def compute_factorial():
-    # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
-
-    # result変数を編集し、最終的な計算結果を保存します
-    result = 0
-
+    number = int(input("please enter a number >= 0:")) #5
+    if number == 0:
+        return 1
+    if number < 0:
+        return "Error: negative numbers are invalid" 
+    for x in range (1, number):
+        number = number * x
+# result変数を編集し、最終的な計算結果を保存します
+    result = number
     return result
 
 
