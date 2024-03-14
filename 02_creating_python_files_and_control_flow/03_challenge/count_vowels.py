2061d275bf66849e7d179d157e1f0d7c195ba429
satomitakahashi <takahashi.satomi@woven.toyota>
2024-03-14 18:28:35 +0900

    submit

--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -1,1 +1,14 @@
-# ここにコードを書いてください
+def count_vowels():
+    userinput = input("Enter a string: ") # ユーザー入力として文字列を受け取る
+    string = userinput.lower() #小文字に変換 hello world!
+    a = string.count('a') # a の数　0
+    b = string.count('e') # e の数　1
+    c = string.count('i') # i の数　0
+    d = string.count('o') # o の数　2
+    e = string.count('u') # u の数　0
+    result = str(a + b + c + d + e)
+
+    print("Enter a string: " + userinput)
+    print("Number of vowels: " + result) 
+
+count_vowels()
