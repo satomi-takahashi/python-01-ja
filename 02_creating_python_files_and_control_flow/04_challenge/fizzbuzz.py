c9b3476878b644ee4ee8d85fa59fd66b37a41ad7
satomitakahashi <takahashi.satomi@woven.toyota>
2024-03-25 16:20:41 +0900

    submit

--- 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
+++ 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
@@ -1,11 +1,11 @@
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:  # 3と5の両方の倍数の場合は 'FizzBuzz'
+    for num in range(1, 101):
+        if num % 3 == 0 and i % 5 == 0:  # 3と5の両方の倍数の場合は 'FizzBuzz'
             print("FizzBuzz")
-        elif i % 3 == 0:  # 3の倍数の場合は 'Fizz'
+        elif num % 3 == 0:  # 3の倍数の場合は 'Fizz'
             print("Fizz")
-        elif i % 5 == 0:  # 5の倍数の場合は 'Buzz'
+        elif num % 5 == 0:  # 5の倍数の場合は 'Buzz'
             print("Buzz")
         else:
-            print(i)
+            print(num)
 fizzbuzz()
